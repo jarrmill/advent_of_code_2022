3a263bef2a90261328f488b872069d102a51343c
Jarrod Miller <jarrmill@outlook.com>
2022-12-02 12:41:14 -0500

    Add day two solution

---
+++ day_two.js
@@ -0,0 +1,80 @@
+const helperElf = require('./helper_elf');
+
+const filename = 'd2.txt';
+
+const calculateScore = function (fileString) {
+  const data = cleanData(fileString);
+  const roundScores = data.map(calculateRoundScore);
+  const sumRoundScores = roundScores.reduce((a, b) => a + b);
+  helperElf.log(`round one is: ${sumRoundScores}`);
+
+  const thrownRoundScores = data.map(throwRound);
+  const sumThrownRoundScores = thrownRoundScores.reduce((a, b) => a + b);
+  helperElf.log(`round two is: ${sumThrownRoundScores}`);
+};
+
+const cleanData = function (fileString) {
+  // input:  string of inputs
+  // output: array of tuples
+  const pairs = fileString.split('\n');
+  return pairs.map(pair => pair.split(' '));
+};
+
+const calculateWinner = function(rpsTuple) {
+  const [elfPlay, yourPlay] = rpsTuple;
+  const winningDict = {
+    'X':'C',
+    'Y':'A',
+    'Z':'B',
+  }
+  const drawDict = {
+    'X':'A',
+    'Y':'B',
+    'Z':'C'
+  }
+
+  if (winningDict[yourPlay] === elfPlay) return 6;
+  if (drawDict[yourPlay] === elfPlay) return 3;
+  return 0;
+};
+
+const calculateChoicePoints = function(choice) {
+  const points = {
+    'X': 1,
+    'Y': 2,
+    'Z': 3
+  }
+
+  return points[choice];
+};
+
+const calculateRoundScore = function(rpsTuple) {
+  return calculateWinner(rpsTuple) + calculateChoicePoints(rpsTuple[1]);
+}
+
+// Part Two
+const xyzDict = {
+  'X': 0,
+  'Y': 3,
+  'Z': 6
+}
+
+const abcDict = {
+  'A': 1,
+  'B': 2,
+  'C': 3
+}
+
+const throwRound = function(rpsTuple) {
+  const [elfPlay, outcome] = rpsTuple;
+  const choices = ['X', 'Y', 'Z'];
+
+  const correctChoice = choices.filter(choice => {
+    return xyzDict[outcome] === calculateWinner([elfPlay, choice]);
+  })[0];
+
+  return xyzDict[outcome] + calculateChoicePoints(correctChoice);
+}
+
+helperElf.readFile(filename)
+  .then(calculateScore);
